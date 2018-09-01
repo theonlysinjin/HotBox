11527a24a55533495bbd391b2ff06a2f85160c21
Sinjin Swanepoel <sinjin.swanepoel@yuppiechef.com>
2018-09-02 00:10:08 +0200

    Add GPLv3

--- secret.h
+++ secret.h
@@ -1,7 +1,8 @@
 // WiFi
-#define WIFI_SSID ""
-#define WIFI_Password ""
+#define WIFI_SSID "Sinjin"
+#define WIFI_Password "0844837676"
 
 // Thingspeak
-#define THINGSPEAK_api ""
-#define THINGSPEAK_talkbackID ""
+#define HOTBOX_api "5WJBWP7CBX4AB1SD"
+#define THINGSPEAK_talkbackID "15076"
+#define THINGSPEAK_talkbackAPI = "DXV72NF66AEQMJJS";
