344a50fdfd06a035a727fb8ec60b6d33b0b874b7
Sinjin Swanepoel <sswanepoel@2u.com>
2021-03-10 18:35:37 +0200

    Some changes

--- secret.h
+++ secret.h
@@ -1,8 +1,8 @@
 // WiFi
-#define WIFI_SSID "Sinjin"
-#define WIFI_Password "0844837676"
+#define WIFI_SSID ""
+#define WIFI_Password ""
 
 // Thingspeak
-#define HOTBOX_api "5WJBWP7CBX4AB1SD"
-#define THINGSPEAK_talkbackID "15076"
-#define THINGSPEAK_talkbackAPI = "DXV72NF66AEQMJJS";
+#define HOTBOX_api ""
+#define THINGSPEAK_talkbackID ""
+#define THINGSPEAK_talkbackAPI = ""
